6a93162bb5bc5a0f05b9b9767918680f3a118d81
IvanStoikov <ivan13.is@abv.bg>
2024-03-30 19:25:43 +0200

    Added class client to the project

---
+++ bookStore/src/Client.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        Socket clientSocket=new Socket("192.168.10.105",1222);
+
+
+
+
+
+    }
+
+}
